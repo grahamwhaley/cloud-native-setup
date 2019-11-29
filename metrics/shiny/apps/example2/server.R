041fd6ad84567a5f97d3be4b897937a21553e49d
Graham Whaley <graham.whaley@intel.com>
2019-11-29 18:08:48 +0000

    metrics: shiny: first cut
    First cut at trying to help display our metrics graphing dynamically
    inside an 'R shiny' thing.
    
    Signed-off-by: Graham Whaley <graham.whaley@intel.com>

---
+++ metrics/shiny/apps/example2/server.R
@@ -0,0 +1,36 @@
+# Define server logic required to draw a histogram ----
+source("/inputdir/Env.R")
+pdf(NULL)
+source(paste(scriptdir, 'collectd_scaling.R', sep='/'))
+
+server <- function(input, output) {
+
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+
+	cpu_line_plot <- ggplot() +
+		geom_line(data=cpuidledata,
+		aes(x=s_offset, y=value, colour=interaction(testname, node),
+		group=interaction(testname, node)),
+		alpha=input$alpha)
+
+	cpu_line_plot
+
+#    x    <- faithful$waiting
+#    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+#    hist(x, breaks = bins, col = "#75AADB", border = "white",
+#         xlab = "Waiting time to next eruption (in mins)",
+#         main = "Histogram of waiting times")
+
+    })
+
+
+}
---
+++ metrics/shiny/apps/example2/ui.R
@@ -0,0 +1,30 @@
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("Hello Shiny!"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Slider for the line alpha blend ----
+      sliderInput(inputId = "alpha",
+                  label = "Line alpha:",
+                  min = 0,
+                  max = 1,
+                  value = 0.8)
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+
+    )
+  )
+)
